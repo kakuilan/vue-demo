a4f3911205822d4fe1ab75d511577e27c2ce3244
kakuilan <kakuilan@163.com>
2019-10-03 22:12:12 +0800

    v-model绑定复选框

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -133,6 +133,23 @@
       <input @keyup.enter="doThis">
     </p>
 
+    <!-- v-model绑定复选框 -->
+    <div>
+      <p>单个复选框：</p>
+      <input type="checkbox" id="checkbox" v-model="checked">
+      <label for="checkbox">{{ checked }}</label>
+        
+      <p>多个复选框：</p>
+      <input type="checkbox" id="runoob" value="Runoob" v-model="checkedNames">
+      <label for="runoob">Runoob</label>
+      <input type="checkbox" id="google" value="Google" v-model="checkedNames">
+      <label for="google">Google</label>
+      <input type="checkbox" id="taobao" value="Taobao" v-model="checkedNames">
+      <label for="taobao">taobao</label>
+      <br>
+      <span>选择的值为: {{ checkedNames }}</span>
+    </div>
+
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
@@ -228,6 +245,8 @@ export default {
         'font-weight': 'bold'
       },
       counter: 0,
+      checked : false,
+      checkedNames: [],
       
     };
   },
