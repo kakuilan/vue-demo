aefcb8f9c7010336d54ab1e002108cc082415cff
kakuilan <kakuilan@163.com>
2019-10-04 19:06:48 +0800

    fix 自定义组件Prop属性验证

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -182,6 +182,11 @@ class="static"
       <custom v-bind:message="parentMsg"></custom>
     </p>
 
+    <!-- 自定义组件Prop属性验证 -->
+    <p>
+      <custom2 propC="必须的属性propC"></custom2>
+    </p>
+
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
@@ -214,13 +219,49 @@ var Child = {
   template: '<h4>自定义组件!{{ message }}</h4>'
 }
 
+var Child2 = {
+  template: '<h4>自定义组件2:{{ propC }}</h4>',
+  props: {
+    // 基础的类型检查 (`null` 和 `undefined` 会通过任何类型验证)
+    propA: Number,
+    // 多个可能的类型
+    propB: [String, Number],
+    // 必填的字符串
+    propC: {
+      type: String,
+      required: true
+    },
+    // 带有默认值的数字
+    propD: {
+      type: Number,
+      default: 100
+    },
+    // 带有默认值的对象
+    propE: {
+      type: Object,
+      // 对象或数组默认值必须从一个工厂函数获取
+      default: function() {
+        return { message: 'hello' }
+      }
+    },
+    // 自定义验证函数
+    propF: {
+      validator: function(value) {
+        // 这个值必须匹配下列字符串中的一个
+        return ['success', 'warning', 'danger'].indexOf(value) !== -1
+      }
+    }
+  }
+}
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
   },
   components: {
-    custom: Child
+    custom: Child,
+    custom2: Child2
   },
   methods: {
     clickEvent: function() {
